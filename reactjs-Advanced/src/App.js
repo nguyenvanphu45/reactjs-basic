16dd7b49a1c03037cd21d0fa2649dde745f3a7cb
Phu <vanphu040520022gmail.com>
2021-10-24 19:15:43 +0700

    upload source code

---
+++ reactjs-Advanced/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Setup from './tutorial/12-memo-useMemo-useCallback'
+
+function App() {
+    return (
+        <div className='container'>
+            <Setup />
+        </div>
+    )
+}
+
+export default App
+
+
+// Link: https://www.youtube.com/watch?v=4UZrsTqkcW4
---
+++ reactjs-Advanced/src/tutorial/1-useState/2-useState-basics.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const UseStateBasics = () => {
+    const [text, setText] = useState('random title');
+
+    const handleClick = () => {
+      if (text === 'random title'){
+        setText("hello world")
+      } else {
+        setText("random title")
+      }
+      
+    }
+    return (
+      <React.Fragment>
+        <h2>{text}</h2>
+        <button className='btn' onClick={handleClick}>
+          change title
+        </button>
+      </React.Fragment>
+    )
+};
+
+export default UseStateBasics;
